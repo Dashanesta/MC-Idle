e1ed77959bbf311ff5cb83770061b929159df29c
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2023-02-26 15:04:09 -0500

    mc assets 2

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    int grass_blocks;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        grass_blocks = 0;
     }
 
     // Update is called once per frame
